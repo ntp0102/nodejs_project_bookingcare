58ed205803222b8d070a425a84cb5213159442ed
ntp0102 <phuc.nguyenthien2000@hcmut.edu.vn>
2023-02-15 17:59:54 +0700

    first commit

---
+++ src/migrations/migration-create-schedule.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const { sequelize } = require("../models");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("schedules", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      currentNumber: {
+        type: Sequelize.INTEGER,
+      },
+      maxNumber: {
+        type: Sequelize.INTEGER,
+      },
+      date: {
+        type: Sequelize.DATE,
+      },
+      doctorId: {
+        type: Sequelize.INTEGER,
+      },
+      timeType: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("schedules");
+  },
+};
---
+++ src/seeders/seeder-demo-user.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: 'phuc@gmail.com',
+        password: '123456',
+        firstName: 'Nguyen',
+        lastName: 'Phuc',
+        address: "HCM",
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole: 'R1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
